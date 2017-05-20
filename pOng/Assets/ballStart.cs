068d0c19f55bdfb9994838e9993871ec84c8321e
forente <zerofox16@gmail.com>
2017-05-19 23:00:29 -0500

    Ball and bounce

---
+++ pOng/Assets/ballStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballStart : MonoBehaviour {
+    public float speed = 30;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+	
+
+}
